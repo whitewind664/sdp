33f63c17e4748cbcfde8c4d78dabab8b279279ff
Kimon Protopapas <kimon.protopapas@epfl.ch>
2021-03-12 01:29:48 +0100

    add board to game activity

--- app/src/main/java/com/github/gogetters/letsgo/GameActivity.kt
+++ app/src/main/java/com/github/gogetters/letsgo/GameActivity.kt
@@ -2,24 +2,46 @@ package com.github.gogetters.letsgo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.github.gogetters.letsgo.game.Board
 import com.github.gogetters.letsgo.game.Game
 import com.github.gogetters.letsgo.game.LocalPlayer
+import com.github.gogetters.letsgo.game.Stone
 
 class GameActivity : AppCompatActivity() {
-    val EXTRA_GAME_SIZE = "com.github.gogetters.letsgo.game.GAME_SIZE"
-    val EXTRA_KOMI = "com.github.gogetters.letsgo.game.KOMI"
+    companion object {
+        const val EXTRA_GAME_SIZE = "com.github.gogetters.letsgo.game.GAME_SIZE"
+        const val EXTRA_KOMI = "com.github.gogetters.letsgo.game.KOMI"
+    }
+
     private lateinit var game: Game
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        val gameSize = intent.getIntExtra(EXTRA_GAME_SIZE, 9)
+        val gameSizeInput = intent.getIntExtra(EXTRA_GAME_SIZE, 9)
         val komi = intent.getDoubleExtra(EXTRA_KOMI, 5.5)
-        val whitePlayer = LocalPlayer(Board.Stone.WHITE)
-        val blackPlayer = LocalPlayer(Board.Stone.BLACK)
-        game = Game(gameSize, komi, whitePlayer, blackPlayer)
+        val boardSize = Board.Size.withSize(gameSizeInput)
 
+        drawBoard(boardSize)
+
+        val whitePlayer = LocalPlayer(Stone.WHITE)
+        val blackPlayer = LocalPlayer(Stone.BLACK)
+        game = Game(boardSize, komi, whitePlayer, blackPlayer)
     }
+
+    private fun drawBoard(boardSize: Board.Size) {
+        val boardImageView = findViewById<ImageView>(R.id.gameBoardImage)
+
+        val boardImage = when (boardSize) {
+            Board.Size.SMALL -> R.drawable.board_9
+            Board.Size.MEDIUM -> R.drawable.board_13
+            Board.Size.LARGE -> R.drawable.board_19
+        }
+
+        boardImageView.setImageResource(boardImage)
+        val gameBoardDimension = boardImageView.width
+    }
+
 }
--- app/src/main/java/com/github/gogetters/letsgo/MainActivity.kt
+++ app/src/main/java/com/github/gogetters/letsgo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.github.gogetters.letsgo
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, GameActivity::class.java).apply {
+            putExtra(GameActivity.EXTRA_GAME_SIZE, 9)
+            putExtra(GameActivity.EXTRA_KOMI, 5.5)
+        }
+
+        startActivity(intent)
     }
 }
