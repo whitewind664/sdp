233b6926b797383324e6a4e4c0e4d89c9473e1ec
remove <erik.wengle@epfl.ch>
2021-03-19 04:15:56 +0100

    add GTPCommandTest

--- app/src/main/java/com/github/gogetters/letsgo/game/GTPCommand.kt
+++ app/src/main/java/com/github/gogetters/letsgo/game/GTPCommand.kt
@@ -7,9 +7,9 @@ sealed class GTPCommand {
     //TODO: turn de/serialize into tostring and tocommand, provide actual serialization
 
     // SETUP COMMANDS -------------------------
-    data class BOARD_SIZE(val size: Board.Size) : GTPCommand(){
+    data class BOARD_SIZE(val boardSize: Board.Size) : GTPCommand(){
         override fun toString(): String {
-            return String.format("boardsize %d", size)
+            return String.format("boardsize %d", boardSize.size)
         }
     }
 
@@ -77,31 +77,33 @@ sealed class GTPCommand {
         return String.format("%d %s", id, toString())
     }
 
+    companion object {
+        fun toCommand(s: String): GTPCommand {
+            val decomposed = s.split(" ")
 
-    fun toCommand(s: String): GTPCommand {
-        val decomposed = s.split(" ")
+            //TODO: create new exception for this case
+            if(decomposed.isEmpty())
+                throw Error("no valid command detected")
 
-        //TODO: create new exception for this case
-        if(decomposed.isEmpty())
-            throw Error("no valid command detected")
+            val command = decomposed[0]
+            val args = decomposed.drop(1)
 
-        val command = decomposed[0]
-        val args = decomposed.drop(1)
+            return when (command) {
+                "boardsize" -> if(args.isEmpty()) throw Error() else return BOARD_SIZE(Board.Size.withSize(args[0].toInt()))
+                "clear_board" -> CLEAR_BOARD
+                "komi" -> if(args.isEmpty()) throw Error() else return KOMI(args[0].toFloat())
+                "fixed_handicap" -> if(args.isEmpty()) throw Error() else return FIXED_HANDICAP(args[0].toInt())
+                "place_free_handicap" -> if(args.isEmpty()) throw Error() else return PLACE_FREE_HANDICAP(args[0].toInt())
+                "set_free_handicap" -> if(args.isEmpty()) throw Error() else TODO("need to convert strings to points")
 
-        return when (command) {
-            "boardsize" -> if(args.isEmpty()) throw Error() else return BOARD_SIZE(Board.Size.withSize(args[1].toInt()))
-            "clear_board" -> CLEAR_BOARD
-            "komi" -> if(args.isEmpty()) throw Error() else return KOMI(args[1].toFloat())
-            "fixed_handicap" -> if(args.isEmpty()) throw Error() else return FIXED_HANDICAP(args[1].toInt())
-            "place_free_handicap" -> if(args.isEmpty()) throw Error() else return PLACE_FREE_HANDICAP(args[1].toInt())
-            "set_free_handicap" -> if(args.isEmpty()) throw Error() else TODO("need to convert strings to points")
-
-            "play" -> TODO("need to create moves from Strings")
-            "genmove" -> TODO("need to create moves from Strings")
-            "undo" -> UNDO
+                "play" -> TODO("need to create moves from Strings")
+                "genmove" -> TODO("need to create moves from Strings")
+                "undo" -> UNDO
 
 
-            else -> throw Error("no valid command detected")
-        }
+                else -> throw Error("no valid command detected")
+            }
+
+    }
     }
 }
