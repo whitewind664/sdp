c2a26c1e5945da72fa0e35435a5b56c33a71595b
Jan Linder <jlind112@gmail.com>
2021-03-22 18:01:08 +0100

    Adjust type of activity

--- app/src/main/java/com/github/gogetters/letsgo/ChatActivity.kt
+++ app/src/main/java/com/github/gogetters/letsgo/ChatActivity.kt
@@ -1,15 +1,14 @@
 package com.github.gogetters.letsgo
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class ChatActivity : WearableActivity() {
+class ChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        // Enables Always-on
-        setAmbientEnabled()
+        // TODO
     }
 }
