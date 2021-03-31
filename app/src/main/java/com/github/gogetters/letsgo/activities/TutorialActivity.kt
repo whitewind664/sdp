9032f2919aa046dce620ee271f17999b87892d58
Jan Linder <jlind112@gmail.com>
2021-03-31 14:07:22 +0200

    Rename game layout according to new converntions

---
+++ app/src/main/java/com/github/gogetters/letsgo/activities/TutorialActivity.kt
@@ -0,0 +1,21 @@
+package com.github.gogetters.letsgo.activities
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.github.gogetters.letsgo.R
+
+class TutorialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
