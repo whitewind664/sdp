8fe28d566be5df3e8ea26b7727db969158ffa233
Jan Linder <jlind112@gmail.com>
2021-04-01 07:44:21 +0200

    Fix main test s.t. it passes locally

--- app/src/androidTest/java/com/github/gogetters/letsgo/activities/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/gogetters/letsgo/activities/MainActivityTest.kt
@@ -10,6 +10,8 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.gogetters.letsgo.R
 import org.hamcrest.Matchers
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,31 +22,31 @@ class MainActivityTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    @Before
+    fun init() {
+        Intents.init()
+        activityRule.scenario.onActivity { activity ->
+            activity.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
+        }
+        Thread.sleep(5000)
+    }
+
+    @After
+    fun cleanUp() {
+        Intents.release()
+        activityRule.scenario.close()
+    }
+
+
     @Test
     fun gameButtonOpensGame() {
-        /** //Somehow doesn't work
-        val scenario = activityRule.scenario
-        scenario.onActivity { activity ->
-            // close system dialogs
-            Thread.sleep(1)
-            activity.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
-
-            Intents.init()
-            onView(withId(R.id.main_button_startGame)).perform(click())
-            Intents.intended(IntentMatchers.hasComponent(GameActivity::class.java.name))
-            Intents.release()
-        }*/
+        onView(withId(R.id.main_button_startGame)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(GameActivity::class.java.name))
     }
 
     @Test
     fun profileButtonOpensProfile() {
-        /** //Somehow doesn't work
-        val scenario = activityRule.scenario
-        scenario.onActivity { activity ->
-            Intents.init()
-            onView(withId(R.id.main_button_profile)).perform(click())
-            Intents.intended(Matchers.allOf(IntentMatchers.hasComponent(ProfileActivity::class.java.name)))
-            Intents.release()
-        }*/
+        onView(withId(R.id.main_button_profile)).perform(click())
+        Intents.intended(Matchers.allOf(IntentMatchers.hasComponent(ProfileActivity::class.java.name)))
     }
 }
