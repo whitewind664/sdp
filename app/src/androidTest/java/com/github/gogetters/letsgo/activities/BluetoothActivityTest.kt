4afb03ceeda40e91a5c905d281fb1976198f56b8
Jan Linder <jlind112@gmail.com>
2021-04-16 21:50:51 +0200

    Add bluetoothActivityTest

---
+++ app/src/androidTest/java/com/github/gogetters/letsgo/activities/BluetoothActivityTest.kt
@@ -0,0 +1,12 @@
+package com.github.gogetters.letsgo.activities
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BluetoothActivityTest {
+    @get:Rule
+    var activityRule = ActivityScenarioRule(ChatActivity::class.java)
+}
