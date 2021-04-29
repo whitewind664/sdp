f9e087132480b44a2180807c36e9ef71bc8635fb
Jan Linder <jlind112@gmail.com>
2021-04-29 09:14:19 +0200

    Introduce titles

--- app/src/main/java/com/github/gogetters/letsgo/activities/TutorialActivity.kt
+++ app/src/main/java/com/github/gogetters/letsgo/activities/TutorialActivity.kt
@@ -21,10 +21,13 @@ import com.github.gogetters.letsgo.tutorial.TutorialLocalPlayer
 import com.github.gogetters.letsgo.tutorial.TutorialPlayer
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlin.math.exp
 
 class TutorialActivity : AppCompatActivity() {
     private var textProgressIndex: Int = 0
-    private val tutorialTextIds = arrayOf(R.string.tutorial_intro, R.string.tutorial_board, R.string.tutorial_board2, R.string.tutorial_stones, R.string.tutorial_stones2, R.string.tutorial_territory, R.string.tutorial_capturing, R.string.tutorial_capturing2, R.string.tutorial_selfCapture, R.string.tutorial_selfCapture2, R.string.tutorial_twoEyes, R.string.tutorial_score1, R.string.tutorial_KoRule, R.string.tutorial_KoRule2, R.string.tutorial_end, R.string.tutorial_score2)
+    private val tutorialTextIds = listOf(R.string.tutorial_intro, R.string.tutorial_board, R.string.tutorial_board2, R.string.tutorial_stones, R.string.tutorial_stones2, R.string.tutorial_territory, R.string.tutorial_capturing, R.string.tutorial_capturing2, R.string.tutorial_selfCapture, R.string.tutorial_selfCapture2, R.string.tutorial_twoEyes, R.string.tutorial_score1, R.string.tutorial_KoRule, R.string.tutorial_KoRule2, R.string.tutorial_end, R.string.tutorial_score2)
+    private var titleProgressIndex: Int = 0
+    private val tutorialTitleIds = listOf(R.string.tutorial_title_intro, R.string.tutorial_title_board, R.string.tutorial_title_stones, R.string.tutorial_title_territories, R.string.tutorial_title_capturing, R.string.tutorial_title_selfCapture, R.string.tutorial_title_score1, R.string.tutorial_title_KoRule, R.string.tutorial_title_end, R.string.tutorial_title_score2, R.string.tutorial_title_outro)
     private var isFinished = false
     private var gameIsRunning = false
 
@@ -33,6 +36,7 @@ class TutorialActivity : AppCompatActivity() {
     private lateinit var boardFrame: FrameLayout
     private lateinit var boardExplanationText: TextView
     private lateinit var boardResetButton: Button
+    private lateinit var explanationTitle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +52,7 @@ class TutorialActivity : AppCompatActivity() {
 
         boardExplanationText = findViewById(R.id.tutorial_textView_boardExplanation)
         boardResetButton = findViewById(R.id.tutorial_button_reset)
+        explanationTitle = findViewById(R.id.tutorial_textView_title)
 
         val localPlayer = TutorialLocalPlayer(inputDelegate)
         val tutorialPlayer = TutorialPlayer()
@@ -103,21 +108,27 @@ class TutorialActivity : AppCompatActivity() {
         boardFrame.visibility = View.VISIBLE
         boardExplanationText.visibility = View.VISIBLE
         boardResetButton.visibility = View.VISIBLE
+        explanationTitle.visibility = View.GONE
     }
 
     private fun hideBoard() {
         boardFrame.visibility = View.GONE
         boardExplanationText.visibility = View.GONE
         boardResetButton.visibility = View.GONE
+        explanationTitle.visibility = View.VISIBLE
         gameIsRunning = false
     }
     
     private fun displayNextText(boardIsDisplayed: Boolean, next: Boolean) {
         val textView = if (boardIsDisplayed) boardExplanationText else findViewById(R.id.tutorial_textView_explanation)
         val nextIndex = if (next) textProgressIndex + 1 else textProgressIndex
-        if (nextIndex < tutorialTextIds.size) {
+        val nextTitleIndex = if (boardIsDisplayed) titleProgressIndex else titleProgressIndex + 1
+
+        if (nextIndex < tutorialTextIds.size && nextTitleIndex < tutorialTitleIds.size) {
             textProgressIndex = nextIndex
+            titleProgressIndex = nextTitleIndex
             textView.text = resources.getString(tutorialTextIds[textProgressIndex])
+            explanationTitle.text = resources.getString(tutorialTitleIds[titleProgressIndex])
         } else {
             if (!isFinished) {
                 // handle end of tutorial
