7ef206ad4734fd7a77b409991960c26d670b7135
smikulas <miksza01@gmail.com>
2021-04-29 22:47:55 +0200

    main menu

--- app/src/main/java/com/github/gogetters/letsgo/activities/MainActivity.kt
+++ app/src/main/java/com/github/gogetters/letsgo/activities/MainActivity.kt
@@ -2,23 +2,25 @@ package com.github.gogetters.letsgo.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.github.gogetters.letsgo.R
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gameButton = findViewById<Button>(R.id.main_button_startGame)
-        gameButton.setOnClickListener {
-            val intent = Intent(this, GameActivity::class.java).apply {
-                putExtra(GameActivity.EXTRA_GAME_SIZE, 9)
-                putExtra(GameActivity.EXTRA_KOMI, 5.5)
-            }
-            startActivity(intent)
-        }
+        val toolBar: Toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolBar)
+        toolBar.setLogo(R.drawable.logo)
+        setupBottomNavigationMenu()
+        setupBottomNavigationLabelVisibilityAndHorizontalTranslation()
 
         val mapButton = findViewById<Button>(R.id.main_button_map)
         mapButton.setOnClickListener {
@@ -32,16 +34,46 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val chatButton = findViewById<Button>(R.id.main_button_chat)
-        chatButton.setOnClickListener {
-            val intent = Intent(this, ChatActivity::class.java)
-            startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_top, menu)
+        return true
+    }
+
+    private fun setupBottomNavigationMenu() {
+        //R.id.bottomNavigation.inflateMenu(R.menu.menu_bottom_navigation_2)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.item1 -> {
+                    val intent = Intent(this, GameActivity::class.java).apply {
+                        putExtra(GameActivity.EXTRA_GAME_SIZE, 9)
+                        putExtra(GameActivity.EXTRA_KOMI, 5.5)
+                    }
+                    startActivity(intent)
+                    true
+                }
+                R.id.item2 -> {
+                    val intent = Intent(this, TutorialActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.item3 -> {
+                    val intent = Intent(this, ChatActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
 
-        val tutorialButton = findViewById<Button>(R.id.main_button_tutorial)
-        tutorialButton.setOnClickListener {
-            val intent = Intent(this, TutorialActivity::class.java)
-            startActivity(intent)
-        }
+        // Use this to programmatically select navigation items
+        //bottomNavigation.selectedItemId = R.id.item1
+    }
+
+    private fun setupBottomNavigationLabelVisibilityAndHorizontalTranslation() {
+        bottomNavigation.labelVisibilityMode = LabelVisibilityMode.LABEL_VISIBILITY_SELECTED
+        //bottomNavigation.isItemHorizontalTranslationEnabled = true
     }
 }
