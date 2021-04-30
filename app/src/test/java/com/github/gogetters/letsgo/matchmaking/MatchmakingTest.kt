b0be2bc86f4183e52b7105a1da4b7c0da2fe1760
whitewind664 <dbajraktarov@hotmail.com>
2021-04-30 05:26:21 +0200

    cirrus testing

---
+++ app/src/test/java/com/github/gogetters/letsgo/matchmaking/MatchmakingTest.kt
@@ -0,0 +1,18 @@
+package com.github.gogetters.letsgo.matchmaking
+
+import org.junit.Test
+import org.junit.Assert.assertTrue
+import kotlin.math.abs
+
+class MatchmakingTest {
+
+    @Test
+    fun eloChangeReturnsExpectedNumbers() {
+        val rating1 = 1200.0
+        val rating2 = 1000.0
+
+        val (newRating1, newRating2) = Matchmaking.eloChange(rating1, rating2, 1)
+        assertTrue(abs(newRating1 - 1207.2) < 0.1)
+        assertTrue(abs(newRating2 - 992.8) < 0.1)
+    }
+}
