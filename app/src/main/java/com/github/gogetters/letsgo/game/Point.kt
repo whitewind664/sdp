ff37fa708906af00da16ca9bc87c1d20971bf290
Kimon Protopapas <kimonroxd@gmail.com>
2021-05-06 23:43:44 +0200

    fix point and color deserialization

--- app/src/main/java/com/github/gogetters/letsgo/game/Point.kt
+++ app/src/main/java/com/github/gogetters/letsgo/game/Point.kt
@@ -1,8 +1,9 @@
 package com.github.gogetters.letsgo.game
 
+private const val offsetASCII = 96
+private const val skippedColumn = 9
+
 data class Point(val first: Int, val second: Int) {
-    private val offsetASCII = 96
-    private val skippedColumn = 9
     operator fun plus(other: Point) = Point(first + other.first, second + other.second)
 
     override fun equals(other: Any?): Boolean {
@@ -26,18 +27,20 @@ data class Point(val first: Int, val second: Int) {
         return col + row
     }
 
-    @ExperimentalStdlibApi
-    fun fromString(string: String): Point {
-        if (string.length != 2) {
-            throw IllegalArgumentException("Incorrectly formatted string")
+    companion object {
+        @ExperimentalStdlibApi
+        fun fromString(string: String): Point {
+            if (string.length != 2) {
+                throw IllegalArgumentException("Incorrectly formatted string")
+            }
+
+            val col = string[0]
+            val row = string[1]
+            val colNumMaybeSkip = (col.toInt() - offsetASCII)
+            val colNum = if (colNumMaybeSkip > skippedColumn) colNumMaybeSkip - 1 else colNumMaybeSkip
+            val rowNum = row.digitToInt()
+
+            return Point(colNum, rowNum)
         }
-
-        val col = string[0]
-        val row = string[1]
-        val colNumMaybeSkip = (col.toInt() - offsetASCII)
-        val colNum = if (colNumMaybeSkip > skippedColumn) colNumMaybeSkip - 1 else colNumMaybeSkip
-        val rowNum = row.digitToInt()
-
-        return Point(colNum, rowNum)
     }
 }
