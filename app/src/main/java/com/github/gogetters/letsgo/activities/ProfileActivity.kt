6e7d0ef70c9ea611d4c3482d8cd977efc9ded52b
Jan Linder <jlind112@gmail.com>
2021-05-06 14:21:16 +0200

    Add display of the profile picture both via camera and via gallery

--- app/src/main/java/com/github/gogetters/letsgo/activities/ProfileActivity.kt
+++ app/src/main/java/com/github/gogetters/letsgo/activities/ProfileActivity.kt
@@ -1,29 +1,41 @@
 package com.github.gogetters.letsgo.activities
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import com.github.gogetters.letsgo.R
 import com.github.gogetters.letsgo.database.UserBundle
 import com.github.gogetters.letsgo.util.PermissionUtils
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ProfileActivity : ActivityCompat.OnRequestPermissionsResultCallback, FirebaseUIActivity() {
     companion object {
+        // Codes used when creating a permission request. Used in the onRequestPermissionResult handler.
         private const val READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE: Int = 2
         private const val WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE: Int = 3
-        private const val IMAGE_CAPTURE_REQUEST_CODE = 4
+        // Codes used when creating an external activity. Used in the onActivityResult handler.
+        private const val CAMERA_ACTIVITY_REQUEST_CODE = 0
+        private const val GALLERY_SELECTION_ACTIVITY_REQUEST_CODE = 1
 
         private const val DIALOG_CAMERA_IDX = 0
         private const val DIALOG_GALLERY_IDX = 1
@@ -36,6 +48,8 @@ class ProfileActivity : ActivityCompat.OnRequestPermissionsResultCallback, Fireb
     private lateinit var emailText: TextView
     private lateinit var cityCountyText: TextView
     private lateinit var saveButton: ImageButton
+
+    private lateinit var profilePictureUri: Uri
 
     /**
      * Defines whether we want to upload a picture from the camera (true) or from the gallery (false, default)
@@ -65,7 +79,12 @@ class ProfileActivity : ActivityCompat.OnRequestPermissionsResultCallback, Fireb
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_ACTIVITY_REQUEST_CODE -> onCameraResult(data)
+                GALLERY_SELECTION_ACTIVITY_REQUEST_CODE -> onGalleryResult(data)
+            }
+        }
         updateUI()
     }
 
@@ -78,9 +97,9 @@ class ProfileActivity : ActivityCompat.OnRequestPermissionsResultCallback, Fireb
             val userBundle = UserBundle(authInstance)
 
             userBundle.letsGo.downloadUserData().addOnCompleteListener {
-                nameText!!.text = userBundle.letsGo.first
-                emailText!!.text = userBundle.firebase.email
-                cityCountyText!!.text = "${userBundle.letsGo.city}, ${userBundle.letsGo.country}"
+                nameText.text = userBundle.letsGo.first
+                emailText.text = userBundle.firebase.email
+                cityCountyText.text = "${userBundle.letsGo.city}, ${userBundle.letsGo.country}"
             }
         }
     }
@@ -97,6 +116,9 @@ class ProfileActivity : ActivityCompat.OnRequestPermissionsResultCallback, Fireb
             PermissionUtils.requestPermission(this,
                     WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            PermissionUtils.requestPermission(this,
+                    READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
             choosePictureFromCamera = clickedIndex == DIALOG_CAMERA_IDX
         })
         builder.show()
@@ -131,15 +153,77 @@ class ProfileActivity : ActivityCompat.OnRequestPermissionsResultCallback, Fireb
     }
 
     private fun dispatchCameraIntent() {
+        profilePictureUri = FileProvider.getUriForFile(this, applicationContext.packageName + ".provider", getOutputImageFile());
+
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, profilePictureUri)
+        takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
         try {
-            startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST_CODE)
+            startActivityForResult(takePictureIntent, CAMERA_ACTIVITY_REQUEST_CODE)
         } catch (e: ActivityNotFoundException) {
             Log.w("PROFILE", "Camera could not be accessed to take profile picture")
         }
     }
 
     private fun dispatchGalleryIntent() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(
+                intent,
+                resources.getString(R.string.profile_gallerySelectionTitle)),
+                GALLERY_SELECTION_ACTIVITY_REQUEST_CODE)
+        Log.i("PROFILE", "activity for gallery started")
+    }
 
+
+    private fun onCameraResult(data: Intent?) {
+        profileImage.setImageURI(profilePictureUri)
+        /**val thumbnail = data.getExtras().get("data")
+        val bytes = ByteArrayOutputStream()
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
+
+        val destination = File(Environment.getExternalStorageDirectory(), System.currentTimeMillis().toString() + ".jpg")
+
+        val fo: FileOutputStream
+        try {
+            destination.createNewFile()
+            fo = FileOutputStream(destination)
+            fo.write(bytes.toByteArray())
+            fo.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        ivImage.setImageBitmap(thumbnail)*/
+    }
+
+    private fun getOutputImageFile(): File {
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        return File.createTempFile("IMG_$timeStamp", ".jpg", storageDir)
+    }
+
+    private fun onGalleryResult(data: Intent?) {
+        if (data != null && data.data != null) {
+            val selectedPhotoUri = data.data
+            try {
+                selectedPhotoUri?.let {
+                    if (Build.VERSION.SDK_INT < 28) {
+                        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedPhotoUri)
+                        profileImage.setImageBitmap(bitmap)
+                    } else {
+                        val source = ImageDecoder.createSource(this.contentResolver, selectedPhotoUri)
+                        val bitmap = ImageDecoder.decodeBitmap(source)
+                        profileImage.setImageBitmap(bitmap)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
