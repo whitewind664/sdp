e51f734582e5af8fdcf60a2855ac8a59f42db06e
Jan Linder <jlind112@gmail.com>
2021-05-19 11:59:32 +0200

    Add interface for the queries

---
+++ app/src/main/java/com/github/gogetters/letsgo/game/util/ogs/OGSCommunicatorService.kt
@@ -0,0 +1,38 @@
+package com.github.gogetters.letsgo.game.util.ogs
+
+import com.github.gogetters.letsgo.game.Move
+
+class OGSCommunicatorService(private val onlineService: OnlineService) : OGSCommunicator {
+    
+    override fun authenticate(username: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAuthenticationAccepted() {
+        TODO("Not yet implemented")
+    }
+
+    override fun startChallenge(ogsGame: OGSGame) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onChallengeAccepted(challengeData: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendMove(move: Move) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReceiveMove(move: Move) {
+        TODO("Not yet implemented")
+    }
+
+    override fun listActiveGames(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun cancelChallenge(challengeID: String) {
+        TODO("Not yet implemented")
+    }
+}
