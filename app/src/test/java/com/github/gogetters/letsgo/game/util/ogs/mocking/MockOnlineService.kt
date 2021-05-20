3f0d5aab692ff7fb3a7f23d079ebecf4f18b5c53
Kimon Protopapas <kimonroxd@gmail.com>
2021-05-20 21:24:03 +0200

    remove customization of challenges

--- app/src/test/java/com/github/gogetters/letsgo/game/util/ogs/mocking/MockOnlineService.kt
+++ app/src/test/java/com/github/gogetters/letsgo/game/util/ogs/mocking/MockOnlineService.kt
@@ -10,7 +10,7 @@ import java.util.*
 
 class MockOnlineService : OnlineService {
     var hasAuthenticated = false
-    var currentGames = listOf(OGSGame("default"), OGSGame("ranked", isRanked = true))
+    var currentGames = listOf(OGSGame("first"), OGSGame("second"))
     var madeMove = false
     var challenges = listOf<OGSChallenge>()
 
@@ -30,51 +30,7 @@ class MockOnlineService : OnlineService {
     }
 
     private fun gameFromJSON(game: JSONObject): OGSGame {
-
-        val rules = when (game.getString("rules")) {
-            OGSGame.RuleType.CHINESE.name.toLowerCase(Locale.ROOT) -> OGSGame.RuleType.CHINESE
-            OGSGame.RuleType.JAPANESE.name.toLowerCase(Locale.ROOT) -> OGSGame.RuleType.CHINESE
-            else -> throw IllegalArgumentException("invalid rule type")
-        }
-
-        val handicap = when (game.getString("handicap")) {
-            OGSGame.HandicapType.AUTOMATIC.toString() -> OGSGame.HandicapType.AUTOMATIC
-            OGSGame.HandicapType.FIXED.toString() -> OGSGame.HandicapType.FIXED
-            OGSGame.HandicapType.NONE.toString() -> OGSGame.HandicapType.NONE
-            else -> throw IllegalArgumentException("invalid handicap type")
-        }
-
-        val timeControlJSON = game.getJSONObject("time_control_parameters")
-        val timeControl = when (timeControlJSON.getString("time_control")) {
-            "none" -> OGSGame.TimeControl.None(timeControlJSON.getInt("total_time"))
-            "byoyomi" -> OGSGame.TimeControl.Byoyomi(
-                timeControlJSON.getInt("main_time"),
-                timeControlJSON.getInt("period_time")
-            )
-            "fischer" -> OGSGame.TimeControl.Fischer(
-                timeControlJSON.getInt("initial_time"),
-                timeControlJSON.getInt("max_time"), timeControlJSON.getInt("time_increment")
-            )
-            "simple" -> OGSGame.TimeControl.Simple(timeControlJSON.getInt("per_move"))
-            "canadian" -> OGSGame.TimeControl.Canadian(
-                timeControlJSON.getInt("main_time"),
-                timeControlJSON.getInt("period_time"), timeControlJSON.getInt("stones_per_period")
-            )
-            "absolute" -> OGSGame.TimeControl.Absolute(timeControlJSON.getInt("total_time"))
-            else -> throw IllegalArgumentException("could not parse time control")
-        }
-
-        return OGSGame(
-            game.getString("name"),
-            rules,
-            game.getBoolean("ranked"),
-            handicap,
-            timeControl,
-            game.getBoolean("pause_on_weekends"),
-            game.getInt("width"),
-            game.getInt("height"),
-            game.getBoolean("disable_analysis")
-        )
+        return OGSGame(game.getString("name"), game.getInt("width"), game.getInt("height"))
     }
 
     override fun post(url: String, body: String): OnlineService.ResponseListener {
