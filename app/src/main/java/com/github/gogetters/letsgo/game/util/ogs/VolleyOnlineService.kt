d45587b98ef45bd1bcfb28fde9cd04d25c2c27c8
Jan Linder <jlind112@gmail.com>
2021-05-21 00:11:14 +0200

    Refactor VolleyOnlineService

--- app/src/main/java/com/github/gogetters/letsgo/game/util/ogs/VolleyOnlineService.kt
+++ app/src/main/java/com/github/gogetters/letsgo/game/util/ogs/VolleyOnlineService.kt
@@ -1,51 +1,24 @@
 package com.github.gogetters.letsgo.game.util.ogs
 
 import android.content.Context
-import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
-import java.io.IOException
 
 class VolleyOnlineService(context: Context) : OnlineService<JSONObject> {
     private val queue: RequestQueue = Volley.newRequestQueue(context)
+    
+    override fun post(url: String, body: JSONObject, headers: JSONObject): ResponseListener<JSONObject> { return getOrPost(url, body, headers) }
 
+    override fun get(url: String, headers: JSONObject): ResponseListener<JSONObject> { return getOrPost(url, null, headers) }
 
-    override fun post(url: String, body: JSONObject, headers: JSONObject): ResponseListener<JSONObject> {
+    private fun getOrPost(url: String, jsonRequest: JSONObject?, headers: JSONObject): ResponseListener<JSONObject> {
         val responseListener = ResponseListener<JSONObject>()
 
-        val jsonRequest = object : JsonObjectRequest(url, body,
-                { response -> responseListener.onResponse(response) },
-                { throw it }) {
-            override fun getHeaders(): MutableMap<String, String> {
-                val map = mutableMapOf<String, String>()
-                for (key in headers.keys()) {
-                    map[key] = headers.getString(key)
-                }
-                return map
-            }
-        }
-
-        queue.add(jsonRequest)
-        return responseListener
-    }
-
-    override fun get(url: String, headers: JSONObject): ResponseListener<JSONObject> {
-        val responseListener = ResponseListener<JSONObject>()
-
-        val jsonRequest = object: JsonObjectRequest(url, null,
-                { response -> responseListener.onResponse(response) },
-                { throw it }) {
-            override fun getHeaders(): MutableMap<String, String> {
-                val map = mutableMapOf<String, String>()
-                for (key in headers.keys()) {
-                    map[key] = headers.getString(key)
-                }
-                return map
-            }
+        val jsonRequest = object: JsonObjectRequest(url, jsonRequest, { response -> responseListener.onResponse(response) }, { throw it }) {
+            override fun getHeaders(): MutableMap<String, String> { return jsonToMap(headers) }
         }
 
         queue.add(jsonRequest)
@@ -56,19 +29,19 @@ class VolleyOnlineService(context: Context) : OnlineService<JSONObject> {
     override fun delete(url: String, headers: JSONObject): ResponseListener<JSONObject> {
         val responseListener = ResponseListener<JSONObject>()
 
-        val stringRequest = object: StringRequest(Method.DELETE, url,
-                { response -> responseListener.onResponse(JSONObject(response)) },
-                { throw it }) {
-            override fun getHeaders(): MutableMap<String, String> {
-                val map = mutableMapOf<String, String>()
-                for (key in headers.keys()) {
-                    map[key] = headers.getString(key)
-                }
-                return map
-            }
+        val stringRequest = object: StringRequest(Method.DELETE, url, { response -> responseListener.onResponse(JSONObject(response)) }, { throw it }) {
+            override fun getHeaders(): MutableMap<String, String> { return jsonToMap(headers) }
         }
 
         queue.add(stringRequest)
         return responseListener
+    }
+
+    private fun jsonToMap(json: JSONObject): MutableMap<String, String> {
+        val map = mutableMapOf<String, String>()
+        for (key in json.keys()) {
+            map[key] = json.getString(key)
+        }
+        return map
     }
 }
