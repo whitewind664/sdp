70efd21c0e46086a84e5ee5b27c9729a8362d1a5
Jan Linder <jlind112@gmail.com>
2021-05-21 08:18:05 +0200

    Correct imports

--- app/src/androidTest/java/com/github/gogetters/letsgo/activities/MapsActivityTest.kt
+++ app/src/androidTest/java/com/github/gogetters/letsgo/activities/MapsActivityTest.kt
@@ -14,10 +14,7 @@ import androidx.test.uiautomator.UiObject
 import androidx.test.uiautomator.UiSelector
 import com.github.gogetters.letsgo.R
 import com.github.gogetters.letsgo.map.mocking.MockLocationSharingService
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import androidx.test.uiautomator.UiObject
 import com.github.gogetters.letsgo.database.EmulatedFirebaseTest
-import com.github.gogetters.letsgo.util.MockLocationSharingService
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
