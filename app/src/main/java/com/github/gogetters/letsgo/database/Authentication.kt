5570b98661c86fa03f83c71652b511e43294b441
whitewind664 <dbajraktarov@hotmail.com>
2021-06-02 16:08:02 +0200

    fix

--- app/src/main/java/com/github/gogetters/letsgo/database/Authentication.kt
+++ app/src/main/java/com/github/gogetters/letsgo/database/Authentication.kt
@@ -11,7 +11,7 @@ class Authentication {
         fun emulatorSettings() {
             // 10.0.2.2 is the special IP address to connect to the 'localhost' of
             // the host computer from an Android emulator.
-            auth.useEmulator("10.0.2.2", 9000)
+            auth.useEmulator("10.0.2.2", 9099)
         }
 
         fun getCurrentUser(): FirebaseUser? {
