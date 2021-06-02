3f7b85a134209734544c43a545595c2aee63b805
Kimon Protopapas <kimonroxd@gmail.com>
2021-06-02 17:21:51 +0200

    add test

--- app/src/androidTest/java/com/github/gogetters/letsgo/activities/GameModeChooserActivityTest.kt
+++ app/src/androidTest/java/com/github/gogetters/letsgo/activities/GameModeChooserActivityTest.kt
@@ -17,7 +17,8 @@ import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiSelector
 import com.github.gogetters.letsgo.R
 import com.github.gogetters.letsgo.database.EmulatedFirebaseTest
-import com.github.gogetters.letsgo.game.util.ogs.VolleyOnlineService
+import com.github.gogetters.letsgo.game.Stone
+import com.github.gogetters.letsgo.game.util.ogs.*
 import junit.framework.Assert.assertEquals
 import org.json.JSONObject
 import org.junit.After
@@ -25,6 +26,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.IllegalStateException
 
 @RunWith(AndroidJUnit4::class)
 class GameModeChooserActivityTest: EmulatedFirebaseTest() {
@@ -78,37 +80,23 @@ class GameModeChooserActivityTest: EmulatedFirebaseTest() {
     }
 
     @Test
-    fun connectionTest() {
+    fun connectionTest() { //TODO remove cause it depends on 3rd party interface
         Log.d("TEST TEST TEST", "here we are not in the activity")
         activityRule.scenario.onActivity { activity ->
-            val service = VolleyOnlineService(activity)
+            val ogs = OGSCommunicatorService(
+                    VolleyOnlineService(activity),
+                    SocketIOService(),
+                    activity.resources.getString(R.string.ogs_client_id),
+                    activity.resources.getString(R.string.ogs_client_secret))
 
-            val url = "https://online-go.com/"
-            val auth = "api/v0/login"
-
-            val clientID = "iwEQK38MjhLnudkiY6vJkrk6roGXks4iSA6fE6ln"
-            val clientSecret = "8L9EX4cFGKyylq9a5rw4aBo4G0vzCtaWdszQdcAex8xr9dMJ46xSLuKqUf" +
-                    "ieKnQydYldq9PgwYeOSsET6XbPLbomniIWh4Bixa5OrIDpfQrpkL7veUiRWz9GFN4NQVRW"
-
-            val username = "kimonroxd" //Don't laugh, this is from my spam email i made when i was 12...
-            val password = "online-go.com" //very secure I know...
-
-            val body = JSONObject()
-
-            body.put("client_id", clientID)
-            body.put("client_secret", clientSecret)
-            body.put("grant_type", "password")
-            body.put("username", username)
-            body.put("password", password)
-
-
-            Log.d("TEST TEST TEST", "SENDING REQUEST TO URL ${url + auth}")
-            val listener = service.post(url + auth, body)
-            listener.setOnResponse {
-                Log.d("RESPONSE RESPONSE RESPONSE", "$it")
-                val returnedName = it.getJSONObject("user").getString("username")
-                assertEquals(username, returnedName)
+            ogs.authenticate("john", "doe").setOnResponse {
+                if (it) {
+                    ogs.startChallenge(OGSChallenge("", OGSGame("", "mygame"), Stone.BLACK))
+                } else {
+                    throw IllegalStateException("FAILED TO AUTHENTICATE")
+                }
             }
+
         }
     }
 
