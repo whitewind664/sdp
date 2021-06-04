a7968a1015a0de9a51b776969b16a396e4b528c9
Jan Linder <jlind112@gmail.com>
2021-06-04 21:53:53 +0200

    Add tests for chatactivity

--- app/src/androidTest/java/com/github/gogetters/letsgo/activities/ChatActivityTest.kt
+++ app/src/androidTest/java/com/github/gogetters/letsgo/activities/ChatActivityTest.kt
@@ -1,31 +1,40 @@
 package com.github.gogetters.letsgo.activities
 
+import android.content.Intent
 import android.widget.EditText
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.gogetters.letsgo.R
 import com.github.gogetters.letsgo.database.EmulatedFirebaseTest
+import com.github.gogetters.letsgo.database.user.FirebaseUserBundleProvider
 import com.github.gogetters.letsgo.database.user.LetsGoUser
 import org.junit.*
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ChatActivityTest: EmulatedFirebaseTest() {
+class ChatActivityTest : EmulatedFirebaseTest() {
 
     val ITEM_IN_TEST = 0
     val USER_IN_TEST = LetsGoUser("id")
 
-    @get:Rule
-    var activityRule = ActivityScenarioRule(ChatActivity::class.java)
+    val intent = Intent(ApplicationProvider.getApplicationContext(), ChatActivity::class.java).putExtra(ChatNewMessageActivity.KEY, USER_IN_TEST)
+    lateinit var scenario: ActivityScenario<ChatActivity>
+
 
     @Before
     fun init() {
         Intents.init()
+        scenario = ActivityScenario.launch(intent)
         USER_IN_TEST.nick = "nick"
         USER_IN_TEST.first = "first"
         USER_IN_TEST.last = "last"
@@ -36,41 +45,25 @@ class ChatActivityTest: EmulatedFirebaseTest() {
     @After
     fun cleanUp() {
         Intents.release()
-        activityRule.scenario.close()
+        scenario.close()
     }
 
-    @Ignore
     @Test
     fun test_isItemsVisible_onAppLaunch() {
         onView(withId(R.id.chat_recyclerview_messages)).check(
-            ViewAssertions.matches(
-                ViewMatchers.isDisplayed()
+            matches(
+                isDisplayed()
             )
         )
-        onView(withId(R.id.chat_editText_input)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(withId(R.id.chat_send_button)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.chat_editText_input)).check(matches(isDisplayed()))
+        onView(withId(R.id.chat_send_button)).check(matches(isDisplayed()))
     }
 
-    @Ignore
     @Test
-    fun test_sendMessagesIsAddedToListView() {
-        val scenario = activityRule.scenario
-        scenario.onActivity { activity ->
-            val testText = "Hello"
-            val entryText: EditText = activity.findViewById(R.id.chat_editText_input)
-            entryText.setText(testText)
-            activity.sendMessage() // argument is not used
-
-            // Check if entry field was emptied
-            //assertTrue(entryText.text.toString().isEmpty())
-
-//          Can't check the following since user not logged in, make mocked user in the future
-
-//          // Check if the message was added to the listview
-//          val listView: ListView = activity.findViewById(R.id.chat_listView_messages)
-//          val item: ChatMessageData = (listView.getItemAtPosition(listView.count - 1) as ChatMessageData)
-//          assertEquals(item.getText(), testText)
-        }
+    fun sentMessageIsAdded() {
+        val message = "message"
+        onView(withId(R.id.chat_editText_input)).perform(ViewActions.typeText(message))
+        onView(withId(R.id.chat_send_button)).perform(click())
+        onView(withText(message)).check(matches(isDisplayed()))
     }
-
 }
